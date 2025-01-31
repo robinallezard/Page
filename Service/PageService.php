c99488745bf3b48593e4f964d1bd61177d03df52
Rob <32717550+robinallezard@users.noreply.github.com>
2025-01-31 11:09:14 +0100

    Update PageService.php

--- Service/PageService.php
+++ Service/PageService.php
@@ -1,98 +1,133 @@
 <?php
 
-/*
- * This file is part of the Thelia package.
- * http://www.thelia.net
- *
- * (c) OpenStudio <info@thelia.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Page\Service;
 
-namespace Page\Twig;
-
-use Page\EventListener\KernelViewListener;
-use Page\Model\PageI18nQuery;
+use InvalidArgumentException;
+use Page\Model\Page as PageModel;
+use Page\Model\PageDocument;
+use Page\Model\PageDocumentQuery;
 use Page\Model\PageQuery;
-use Propel\Runtime\ActiveQuery\Criteria;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Thelia\Service\Model\LangService;
-use TheliaBlocks\Service\JsonBlockService;
-use TheliaLibrary\Model\LibraryItemImageQuery;
-use TheliaLibrary\Service\LibraryImageService;
-use TheliaLibrary\Twig\LibraryImage;
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
+use Page\Page;
+use Propel\Runtime\Exception\PropelException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Thelia\Core\Template\Parser\ParserResolver;
+use Thelia\Core\Template\ParserInterface;
+use Thelia\Core\Template\TemplateHelperInterface;
+use Thelia\Core\Translation\Translator;
 
-class PageTwigExtension extends AbstractExtension
+class PageService
 {
-    public function __construct(
-        private JsonBlockService $jsonBlockService,
-        private LibraryImageService $libraryImageService,
-        private LibraryImage $theliaLibraryTwigImage,
-        protected RequestStack $requestStack,
-        private LangService $localeService,
-    ) {}
+    protected ParserInterface $parser;
 
-    public function getFunctions(): array
+    public function __construct(protected ParserResolver $parserResolver, protected TemplateHelperInterface $templateHelper) {}
+
+    public function getPageTemplateName(PageModel $page, bool $readCode = true)
     {
-        return [
-            new TwigFunction('getPageData', [$this, 'getPageData']),
-            new TwigFunction('getPageList', [$this, 'getPageList'])
-        ];
+
+        if ($page->isHome()) {
+            $templateName = 'page-home';
+        } else if ($readCode) {
+            $templateName = 'page-' . $page->getCode();
+        } else if ($page->getPageType()) {
+            $templateName = 'page-' . $page->getPageType()->getType();
+        }
+
+        if ($templateName) {
+            $path = $this->templateHelper->getActiveFrontTemplate()->getAbsolutePath();
+            $this->parser = $this->parserResolver->getParser($path, $templateName);
+
+            $filePath = $path . DS . $templateName . '.' . $this->parser->getFileExtension();
+
+            if (file_exists($filePath)) {
+                return $templateName;
+            }
+        }
+
+        $parent = $page->getParent();
+
+        if (null === $parent) {
+            return null;
+        }
+
+        return $this->getPageTemplateName($parent, true);
     }
 
-    public function getPageData()
+
+    /**
+     * @param $pageId
+     * @return \Page\Model\Page
+     */
+    public function getPageData($pageId): \Page\Model\Page
     {
-        return KernelViewListener::$page;
+        $page = PageQuery::create()
+            ->filterById($pageId)
+            ->findOne();
+
+        if (!$page) {
+            throw new InvalidArgumentException(Translator::getInstance()->trans('Page not found', [], Page::DOMAIN_NAME));
+        }
+
+        return $page;
     }
 
-    public function getPageList(?array $params = [])
+    /**
+     * @param UploadedFile $uploadedFile
+     * @param int $pageId
+     * @param string $locale
+     * @return PageDocument
+     * @throws PropelException
+     */
+    public function savePageDocument(UploadedFile $uploadedFile, int $pageId, string $locale = 'en_US'): PageDocument
     {
-        $currentLocale = $this->localeService->getLocale();
+        $page = PageQuery::create()->findPk($pageId);
 
-        $query = PageQuery::create();
-
-        if (array_key_exists('parent_tree_level', $params)) {
-            $query->filterByTreeLevel($params['parent_tree_level']);
-        }
-        if (array_key_exists('code', $params)) {
-            $query->filterByCode($params['code']);
-        }
-        if (array_key_exists('id', $params)) {
-            $query->filterById($params['id']);
-        }
-        if (array_key_exists('exclude_id', $params)) {
-            $query->filterById($params['exclude_id'], Criteria::NOT_IN);
-        }
-        if (array_key_exists('tag', $params)) {
-            $query
-                ->usePageTagCombinationQuery()
-                ->usePageTagQuery()
-                ->filterByTag($params['tag'], Criteria::IN)
-                ->endUse()
-                ->endUse();
+        if (!$page) {
+            throw new InvalidArgumentException(Translator::getInstance()->trans('Page not found', [], Page::DOMAIN_NAME));
         }
 
-        $pages = $query
-            ->filterByVisible(1)
-            ->filterByIsHome(0);
+        $pageDocument = PageDocumentQuery::create()
+            ->usePageDocumentI18nQuery()
+            ->filterByLocale($locale)
+            ->filterByFile($uploadedFile->getFilename())
+            ->endUse()
+            ->findOne();
 
-        $results = [];
-
-        foreach ($pages as $page) {
-            $pageI18nQuery = PageI18nQuery::create();
-            $imageTitle = $pageI18nQuery->filterById($page->getId())->findOne();
-
-            $results[] = [
-                'CODE' => $page->getCode(),
-                'ID' => $page->getId(),
-                'TITLE' =>  $imageTitle->getTitle(),
-                'URL' => $page->getUrl($currentLocale),
-            ];
+        if (!$pageDocument) {
+            $pageDocument = new PageDocument();
+            $pageDocument->setVisible(1);
         }
 
-        return $results;
+        $pageDocument
+            ->setPageId($pageId)
+            ->setLocale($locale)
+            ->setFile($uploadedFile->getFilename())
+            ->setTitle($uploadedFile->getFilename())
+            ->save();
+
+        return $pageDocument;
+    }
+
+
+    /**
+     * @param string $mode
+     * @param int $pageId
+     * @param int|null $position
+     * @return void
+     */
+    public function changePosition(string $mode, int $pageId, int $position = null): void
+    {
+        if (null !== $page = PageQuery::create()->findPk($pageId)) {
+            switch ($mode) {
+                case 'down':
+                    $page->moveToNextSiblingOf($page->getNextSibling());
+                    break;
+                case 'up':
+                    $page->moveToPrevSiblingOf($page->getPrevSibling());
+                    break;
+                default:
+                    $page->changeAbsolutePosition($position);
+                    break;
+            }
+        }
     }
 }
